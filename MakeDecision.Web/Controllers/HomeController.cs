7c6eb848f03e97666a94d53ceecf8e67954b96ee
chris.zen <fzhenmei@gmail.com>
2012-06-18 23:13:18 +0800

    the home page
    create home page data load funcations

--- MakeDecision.Web/Controllers/HomeController.cs
+++ MakeDecision.Web/Controllers/HomeController.cs
@@ -1,14 +1,31 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using MakeDecision.Web.Models;
 
 namespace MakeDecision.Web.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        private readonly IDepartmentUserRepository departmentUserRepository;
+        private readonly IKeyDataRepository keyDataRepository;
+
+        public HomeController() : this(new DepartmentUserRepository(), new KeyDataRepository())
+        {
+        }
+
+        public HomeController(IDepartmentUserRepository departmentUserRepository, IKeyDataRepository keyDataRepository)
+        {
+            this.departmentUserRepository = departmentUserRepository;
+            this.keyDataRepository = keyDataRepository;
+        }
+
         public ActionResult Index()
         {
-            //ViewBag.Message = "欢迎使用领导决策系统数据收集";
-
+            IQueryable<DepartmentUser> departmentUser =
+                departmentUserRepository.AllIncluding(du => du.Department, du => du.Department.Categories).Where(
+                    du => du.UserName == User.Identity.Name);
+            //var keyDatas = keyDataRepository.All.Where(k => departmentUser.Select(c => c.Department.Categories.Select(ca => ca.Id)).con);
             return Redirect("/KeyData");
         }
 
